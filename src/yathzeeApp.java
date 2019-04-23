88eb442c4ad1e8261a0da0edd230624db0b4a1b9
MarkLunding <mark@lunding.nl>
2019-04-23 22:02:12 +0200

    Doelstelling 1: Loop in de psmv

--- src/yathzeeApp.java
+++ src/yathzeeApp.java
@@ -1,12 +1,27 @@
+import java.util.Scanner;
 
 public class yathzeeApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		boolean playOn = true;
+		while (playOn) {
+			System.out.println("Geef een letter op: q = Quit");
+			String invoer = scanner.nextLine();
+			invoer = invoer.toLowerCase();
+			switch (invoer) {
+			case "q":
+				playOn = false;
+				break;
 
+			default:
+				break;
+			}
+		}
+		System.out.println("einde");
+		scanner.close();
 	}
 
-}
-class dice{
-	
+
+
 }
