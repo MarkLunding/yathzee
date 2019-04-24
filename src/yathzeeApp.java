1ea83fed8c0dfd818c408782e6a58fd0cebc3895
MarkLunding <mark@lunding.nl>
2019-04-24 12:57:25 +0200

    Doelstelling 7: printen worp in eigen class

--- src/yathzeeApp.java
+++ src/yathzeeApp.java
@@ -13,7 +13,7 @@ public class yathzeeApp {
 
 class YahtzeeSpel {
 	ArrayList<Dobbelsteen> dobbelstenen = new ArrayList<>();
-	int [] blokkeer = {0,0,0,0,0};
+	int[] blokkeer = { 0, 0, 0, 0, 0 };
 
 	YahtzeeSpel() {
 		for (int i = 0; i < 5; i++) {
@@ -25,47 +25,66 @@ class YahtzeeSpel {
 		Scanner scanner = new Scanner(System.in);
 		boolean playOn = true;
 		while (playOn) {
-			System.out.println("Geef een letter op: q = Quit");
+			System.out.println("Druk op <ENTER> voor een worp. Stop het spel met q (van Quit)");
 			String invoer = scanner.nextLine();
 			invoer = invoer.toLowerCase();
 			switch (invoer) {
 			case "q":
+			case "Q":
 				playOn = false;
 				break;
-			default:
-				System.out.print("| ");
+			case "":
+				int x = 0;
+				Worp worp = new Worp();
 				for (Dobbelsteen dobbelsteen : dobbelstenen) {
-					dobbelsteen.waarde = dobbelsteen.werpen();
-					System.out.print(dobbelsteen.waarde + " | ");
+					if (blokkeer[x] == 0) {
+						dobbelsteen.waarde = dobbelsteen.werpen();
+						worp.worp[x] = dobbelsteen.waarde;
+					} else {
+						worp.worp[x] = dobbelsteen.waarde;
+					}
+
+					x++;
 				}
-				System.out.println();
+				worp.toonWorp();
+
 				vasthouden();
 				break;
+			default:
+				System.out.println("Invoer ongeldig.");
+
 			}
-			
+
 		}
 		System.out.println("einde");
 
-		scanner.close();
 	}
-	void vasthouden () {
-//		ArrayList<Integer> teBlokkeren = new ArrayList<>();
+
+	void vasthouden() {
+		resetVasthouden();
 		Scanner blokscan = new Scanner(System.in);
-		System.out.println("Welke posities wilt u vasthouden? 0 voor geen anders bv 124"); 
+		System.out.println("Welke posities wilt u vasthouden? 0 voor geen anders bv 124");
 		String invoer = blokscan.nextLine();
 		boolean juisteInvoer = true;
-		for (int i = 0; i <invoer.length(); i++){
+		for (int i = 0; i < invoer.length(); i++) {
 			int getal = Character.getNumericValue(invoer.charAt(i));
-			System.out.println(getal);
-			if(getal>0 && getal<6) {
-				blokkeer[getal-1] = 1;
-			} else {
+			if (getal > 0 && getal < 6) {
+				blokkeer[getal - 1] = 1;
+			} else if (getal > 5) {
 				System.out.println("De waarde " + getal + "is geen geldige postie. Voer waardes in van 1 t/m 5:");
 				juisteInvoer = false;
+
 			}
 		}
 		if (!juisteInvoer) {
 			this.vasthouden();
+		}
+
+	}
+
+	void resetVasthouden() {
+		for (int i = 0; i < blokkeer.length; i++) {
+			blokkeer[i] = 0;
 		}
 	}
 
@@ -77,6 +96,26 @@ class Dobbelsteen {
 	int werpen() {
 		Random random = new Random();
 		return random.nextInt(6) + 1;
+	}
+
+}
+
+class Worp {
+	int[] worp = new int[5];
+
+	void toonWorp() {
+		String underScores = "_______________________________";
+		System.out.println(underScores);
+		System.out.println("| Positie | 1 | 2 | 3 | 4 | 5 |");
+		System.out.print("| Waardes | ");
+		for (int i = 0; i < worp.length; i++) {
+			System.out.print(worp[i] + " | ");
+		}
+		System.out.println();
+		for (int i = 0; i < underScores.length(); i++) {
+			System.out.print((char) '\u203E');
+		}
+		System.out.println();
 	}
 
 }
